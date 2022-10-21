d08c21b49361fd5278722325218f37fcd83a0c52
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2022-10-22 01:52:13 +0300

    Update Runner.java

--- task_1/src/Runner.java
+++ task_1/src/Runner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Полстяной Игорь");
+        System.out.println("Введите свое фамилию и имя");
         String name = scanner.nextLine();
         Task_1 task = new Task_1();
         Task_1_Tests.test(task, name);
